d56173509eb41918a31548fd402a825f9a19fb23
Thành Lợi <dinhhoangloibt@gmail.com>
2025-11-19 19:02:21 +0700

    Refactor code: create routes, app/controller foldes

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const path = require("path");
-const { engine } = require('express-handlebars')
+const { engine } = require("express-handlebars");
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const route = require("./routes");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
 app.use(morgan("combined"));
 
 // Template engine
-app.engine("hbs", engine({
-  extname: '.hbs'
-}));
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, 'resources', 'views'));
+app.set("views", path.join(__dirname, "resources", "views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
